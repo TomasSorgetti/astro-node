a383d704da85789e3aa30738162c91d43216e8ac
TomasSorgetti <tomassorgetti456@gmail.com>
2024-12-13 01:03:51 -0300

    git test

--- server.js
+++ server.js
@@ -2,5 +2,7 @@ import app from "./src/app.js";
 const PORT = 8080;
 
 app.listen(PORT, () => {
+  console.log("- - - - - - - - - - - - - - - - -");
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log("- - - - - - - - - - - - - - - - -");
 });
